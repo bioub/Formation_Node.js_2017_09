ea7683589f3953b30e8c2e40fe87dd03c1890017
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-09-12 16:00:10 +0200

    Objet

---
+++ Langage/05-object.js
@@ -0,0 +1,66 @@
+// Reference
+let p1 = 'Jean';
+let p2 = p1; // number et boolean: passage par valeur
+p2 = 'Eric';
+console.log(p1); // 'Jean'
+
+let o1 = {
+  p: 'Jean'
+};
+let o2 = o1; // passage par référence
+o2.prenom = 'Eric';
+console.log(o1.prenom); // 'Eric'
+
+const coords = {
+  x: 10,
+  y: 20,
+};
+
+Math.pow(
+  2,
+  3,
+);
+
+const createButton = function (options) {
+  const valeur = options.valeur || 'Sans titre';
+  const largeur = options.largeur || 100;
+};
+
+createButton({
+  largeur: 300,
+});
+
+const Voiture = function(marque, modele) {
+  this.modele = modele;
+
+  this.getInfo = function() {
+    return 'Voiture ' + marque + ' ' + modele;
+  };
+};
+
+const clio = new Voiture('Renault', 'Clio');
+console.log(typeof Voiture); // function
+console.log(typeof clio); // object
+console.log(clio.modele); // Clio
+console.log(clio.getInfo()); //Voiture Renault Clio
+
+const p208 = new Voiture('Peugeot', '208');
+console.log(p208.getInfo()); // Voiture Peugeot 208
+
+console.log(p208.getInfo === clio.getInfo); // false
+
+
+const Contact = function(prenom) {
+  this._prenom = prenom;
+};
+
+Contact.prototype.hello = function() {
+  return 'Bonjour je suis ' + this.prenom;
+};
+
+const romain = new Contact('Romain');
+console.log(romain._prenom);
+console.log(romain.hello());
+console.log(romain.hasOwnProperty('_prenom')); // true
+console.log(romain.hasOwnProperty('hello')); // false
+
